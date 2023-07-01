f8c7c295aa4e3cbf525166df537019b16a45aee9
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-07-01 21:21:15 +0600

    general details complete

--- src/Components/UpdateProfile/GeneralDetails.jsx
+++ src/Components/UpdateProfile/GeneralDetails.jsx
@@ -2,19 +2,19 @@ import { Link } from 'react-router-dom';
 import './GeneralDetails.css'
 const GeneralDetails = () => {
     return (
-        <div className="details-bg">
-            <div>
+        <div className="details-bg py-2 padding-right">
+            <div >
                 <div style={{ padding: '15px' }} className='d-flex justify-content-between align-items-center'>
                     <h1 className='fs-6 fw-semibold'>Full Name</h1>
                     <h2 className='fs-6 fw-normal'>Mr. ANUP ROY</h2>
-                    <Link className='edite-link'>Edite</Link>
+                    <Link className='edite-link  '>Edite</Link>
 
                 </div>
 
 
                 <div style={{ padding: '15px' }} className='d-flex align-items-center'>
                     <h1 className='fs-6 margin-right  fw-semibold'>Profile Picture</h1>
-                   
+
                     <Link className='edite-link'>Update</Link>
 
                 </div>
@@ -35,13 +35,23 @@ const GeneralDetails = () => {
                 </div>
 
 
-                
+
                 <div style={{ padding: '15px' }} className='d-flex align-items-center'>
-                    <h1 className='fs-6 margin-right  fw-semibold'>Reffered Id</h1>
+                    <h1 className='fs-6 reffered-margin-right  fw-semibold'>Reffered Id</h1>
                     <h2 className='fs-6 fw-normal'>REF1234ABCD</h2>
 
                 </div>
 
+            </div>
+
+
+            <div className='text-center p-2'>
+                <button className='btn btn-cancel '>
+                    Cancle
+                </button>
+                <button className='btn btn-primary ms-2'>
+                    Save And Next
+                </button>
             </div>
         </div>
     );
